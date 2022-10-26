8bd7d2b292f0579d183745f1cd8412c70e96679b
Xavier Cruz <37046592+sujyrokimora@users.noreply.github.com>
2022-10-26 09:38:40 +0100

    Create func_recursiva_teste.ino

---
+++ func_recursiva_teste.ino
@@ -0,0 +1,27 @@
+
+void setup() {
+ Serial.begin(9600);
+ Serial.flush();
+ Serial.print(func(12)); 
+
+}
+
+void loop() {
+
+}
+
+int func(int n)
+{
+  
+  double vfat;
+  
+  if ( n <= 1 )
+    //Caso base: fatorial de n <= 1 retorna 1
+    return (1);
+  else
+  {
+    //Chamada recursiva
+    vfat = n * func(n - 1);
+    return (vfat);
+  }
+}
